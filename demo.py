bbc3c5e674946ab54e812c42beb4e414c2f24e6e
admin <admin@localhost.localdomain>
2019-09-20 16:08:21 -0400

    adding demo.py

---
+++ demo.py
@@ -0,0 +1,5 @@
+import os
+print("----------------------------------------------------------------------")
+print("proof of concept - don't echo or print secrets in real life :)")
+print("DB_PASSWORD: " + os.environ['DB_PASSWORD'])
+print("----------------------------------------------------------------------")
